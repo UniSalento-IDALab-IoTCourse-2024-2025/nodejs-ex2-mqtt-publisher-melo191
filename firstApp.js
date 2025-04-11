e2cb26f88b2cf771bcc8dbd0aae4781017726bc4
melo191 <carmelomandrino@gmail.com>
2025-04-11 13:15:38 +0200

    mqtt

--- firstApp.js
+++ firstApp.js
@@ -1,16 +1,12 @@
 //const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
-const http = require('node:http');
-
-// Setup sensor, exit if failed
-//var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
-//var sensorPin = 4; // The GPIO pin number for sensor signal
-//if (!sensorLib.initialize(sensorType, sensorPin))
-//{
-//print a warning message in the console
-//console.warn('Failed to initialize sensor');
-//process.exit(1);
-//}
-
+const mqtt=require('mqtt');
+var client = mqtt.connect("mqtt://mqtt.eclipseprojects.io",{clientId:"mqttjs01"}); 
+client.on("connect",function(){
+  console.log("connected");
+});
+client.on("error",function(error){
+  console.log("Can't connect"+error);
+});
 // Automatically update sensor value every 2 seconds
 //we use a nested function (function inside another function)
 setInterval(function() {
@@ -36,21 +32,5 @@ setInterval(function() {
 	},
 	};
 
-	const req = http.request(options, (res) => {
-		res.setEncoding('utf8');
-		res.on('data', (chunk) => {
-		  console.log(`BODY: ${chunk}`);
-		});
-		res.on('end', () => {
-		  console.log('No more data in response.');
-		});
-	});
-
-	req.on('error', (e) => {
-		console.error(`problem with request: ${e.message}`);
-	});
-
-	// Write data to request body
-	req.write(postData);
-	req.end();
+	client.publish("test-topic-handson/data", postData);
 }, 2000);
